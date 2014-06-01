d32fd2c2e32c569f9fc142a2c82f28380e1ed816
rcn-neo-yang <rcn.neo.yang@gmail.com>
2014-06-01 22:56:37 +0800

    change for rspec autotest pass

--- spec/views/pages/contact.html.erb_spec.rb
+++ spec/views/pages/contact.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe "pages/contact.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "displays the text for autotest" do
+    render
+    rendered.should contain("Hello world!")
+  end
 end
