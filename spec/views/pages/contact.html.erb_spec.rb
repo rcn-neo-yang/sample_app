0667acfbd4b67855cbbfb65c74da529531542671
rcn-neo-yang <rcn.neo.yang@gmail.com>
2014-06-01 23:27:49 +0800

    follow 3.2.2 remove spec/views/pages/*.* and spec/helpers/*.*. Rename .autotest to ..aautotest because runing autotest command show error, only runing autotest -rails OK

--- spec/views/pages/contact.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "pages/contact.html.erb" do
-  #pending "add some examples to (or delete) #{__FILE__}"
-  it "displays the text for autotest" do
-    render
-    rendered.should contain("Hello world!")
-  end
-end
