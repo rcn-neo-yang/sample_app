9d80c8d77c7b9fe28881d7c996a710cb39cb2b5c
rcn-neo-yang <rcn.neo.yang@gmail.com>
2014-06-17 15:46:12 +0800

    complete chapter 3

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,11 @@
-require 'spec_helper'
-
 describe PagesController do
+ 
+  render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+	
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -9,11 +13,49 @@ describe PagesController do
     end
   end
 
+  it "should have the right title" do 
+    get 'home'
+    response.should have_selector("title",
+			:content => @base_title + " | Home")
+  end
+
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
       response.should be_success
     end
+  end
+
+  it "should have the right title" do 
+    get 'contact'
+    response.should have_selector("title",
+			:content => @base_title + " | Contact")
+  end
+
+  describe "GET 'about'" do 
+    it "returns http success" do
+      get 'about' 
+      response.should be_success
+    end
+  end
+
+  it "should have the right title" do 
+    get 'about'
+    response.should have_selector("title",
+			:content => @base_title + " | About")
+  end
+
+  describe "GET 'help'" do 
+    it "returns http success" do
+      get 'help' 
+      response.should be_success
+    end
+  end
+
+  it "should have the right title" do 
+    get 'help'
+    response.should have_selector("title",
+			:content => @base_title + " | Help")
   end
 
 end
